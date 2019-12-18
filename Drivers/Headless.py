64985d22abf73f6fb06a15b8e1bcb80807efe0d3
Andres Haro <hvaandres@icloud.com>
2019-12-18 14:56:37 -0700

    Chrome Driver Files
    These files will help you to run your test with chrome. By grabbing these files, you will need to take a look at the chrome driver version that you will need. Also, you will need to make executable your chrome-driver. In this way, you will be able to get the right path.

---
+++ Drivers/Headless.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import threading
+import time
+
+'''
+Please remember to change the chrome driver path
+
+'''
+
+def selenium_drivers():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome(executable_path="/home/oem/Desktop/Coding/Selenium/Drivers78/chromedriver")
+    url = 'https://ampdemo.azureedge.net/azuremediaplayer.html?url=%2F%2Famssamples.streaming.mediaservices.windows.net%2F49b57c87-f5f3-48b3-ba22-c55cfdffa9cb%2FSintel.ism%2Fmanifest&muted=true&aes=true&wallClockDisplayEnabled=true&useLocalTimeZone=true'
+    driver.get(url)
+    
+    time.sleep(200)
+    driver.quit()
+
+Nubmer = 100   # Number of browsers to be open
+thread_list = list()
+
+# Testing begins
+for i in range(Nubmer):
+    t = threading.Thread(name='Test {}'.format(i), target=selenium_drivers)
+    t.start()
+    time.sleep(1)
+    print(t.name + ' started!')
+    thread_list.append(t)
+
+# Wait for all thre<ads to complete
+for thread in thread_list:
+    thread.join()
+
+print('The test is already done')
