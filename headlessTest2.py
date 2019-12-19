d96cd41debdee608a0b5c860dcbb364c3e7bbd1b
Andres Haro <hvaandres@icloud.com>
2019-12-19 15:08:42 -0700

    Update headlessTest2.py

--- headlessTest2.py
+++ headlessTest2.py
@@ -17,7 +17,7 @@ time.sleep(2)
 driver.get('https://ampdemo.azureedge.net/azuremediaplayer.html?url=%2F%2Famssamples.streaming.mediaservices.windows.net%2F49b57c87-f5f3-48b3-ba22-c55cfdffa9cb%2FSintel.ism%2Fmanifest&muted=true&aes=true&wallClockDisplayEnabled=true&useLocalTimeZone=true') 
 print(driver.title)
 
-time.sleep(2)
+time.sleep(200)
 
 Number = 50
 thread_list = list()
